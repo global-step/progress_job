858d9707c794fa740d28d3669f792d4bd542a217
Shohei Kameda <shoheik@cpan.org>
2017-12-13 16:18:43 +0900

    added handling of nil job

--- app/controllers/progress_job/progress_controller.rb
+++ app/controllers/progress_job/progress_controller.rb
@@ -3,9 +3,12 @@ module ProgressJob
 
     def show
       @delayed_job = Delayed::Job.find_by_id(params[:job_id])
-      return unless @delayed_job.present?
-      percentage = !@delayed_job.progress_max.zero? ? @delayed_job.progress_current / @delayed_job.progress_max.to_f * 100 : 0
-      render json: @delayed_job.attributes.merge!(percentage: percentage).to_json
+      if @delayed_job.present?
+        percentage = !@delayed_job.progress_max.zero? ? @delayed_job.progress_current / @delayed_job.progress_max.to_f * 100 : 0
+        render json: @delayed_job.attributes.merge!(percentage: percentage).to_json
+      else
+        render json: {error: '404'}, status: 404
+      end 
     end
 
   end
