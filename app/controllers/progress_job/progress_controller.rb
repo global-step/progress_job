c577f342187f5aadc453578c6eb57954ec3e62ef
shohey1226 <shoheik@cpan.org>
2020-05-18 03:24:51 +0000

    chck progress_max

--- app/controllers/progress_job/progress_controller.rb
+++ app/controllers/progress_job/progress_controller.rb
@@ -3,12 +3,12 @@ module ProgressJob
 
     def show
       @delayed_job = Delayed::Job.find_by_id(params[:job_id])
-      if @delayed_job.present?
+      if @delayed_job.present? && @delayed_job.progress_max.present?
         percentage = !@delayed_job.progress_max.zero? ? @delayed_job.progress_current / @delayed_job.progress_max.to_f * 100 : 0
         render json: @delayed_job.attributes.merge!(percentage: percentage).to_json
       else
         render json: {error: '404'}, status: 404
-      end 
+      end
     end
 
   end
